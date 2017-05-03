c1c2de7f1bbd35bf350457dd0b3b6ef952550723
wltjr0920 <wltjr0920@ajou.ac.kr>
2017-05-03 14:09:19 +0900

    dd

---
+++ serin/src/serin/Laptop.java
@@ -0,0 +1,43 @@
+package serin;
+
+public class Laptop implements Networked{
+	private double totalCapacity;
+	private double usedCapacity;
+	
+	public double getFreeCapacity(){
+		return 0;
+	}
+	
+	public void format(){
+		
+	}
+	
+	public boolean isConnected(){
+		return false;
+	}
+	
+	public double maxSpeed(){
+		return 0;
+	}
+	
+	public double getTotalCapacity() {
+		return totalCapacity;
+	}
+
+	public void setTotalCapacity(double totalCapacity) {
+		this.totalCapacity = totalCapacity;
+	}
+
+	public double getUsedCapacity() {
+		return usedCapacity;
+	}
+
+	public void setUsedCapacity(double usedCapacity) {
+		this.usedCapacity = usedCapacity;
+	}
+
+	public String getName(){
+		return null;
+	}
+}
+
---
+++ serin/src/serin/Networked.java
@@ -0,0 +1,5 @@
+package serin;
+
+public interface Networked {
+
+}
