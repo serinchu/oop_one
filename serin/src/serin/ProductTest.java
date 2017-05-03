9aba95f04455413ec3a2b41aaa7bca7cbc9cd45a
ChoeJiwon <jiwon0929@ajou.ac.kr>
2017-05-03 17:01:39 +0900

    ㅗㅑ

--- serin/src/serin/ProductTest.java
+++ serin/src/serin/ProductTest.java
@@ -4,7 +4,27 @@ public class ProductTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Product P = new Product();
+		
 	}
+	
+	public void testProduct(Product){
+		
+	}
+	
+	public void testDataStorage(DataStorage){
+		
+	}
+	
+	public void testNetworked(Networked){
+		
+	}
+	
+	public void testCooker(Cooker){
+		
+	}
+	
+	
 
 }
--- serin/src/serin/SmartTV.java
+++ serin/src/serin/SmartTV.java
@@ -2,10 +2,30 @@ package serin;
 
 public class SmartTV extends Product {
 
+	int channel;
+	
+	public boolean isConnected(){
+		
+		return true;
+	}
+	
+	public double maxSpeed(){
+	
+		return 0;
+	}
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+	
 
 }
